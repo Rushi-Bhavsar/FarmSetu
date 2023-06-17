ab5d39e2cc6a90e80ac96b46e65508f47b0f9cc7
Rushikesh Bhavsar <rushikesh.bhavsar@bajajfinserv.in>
2023-06-17 20:23:06 +0530

    Adding basic admin page.

--- weather/admin.py
+++ weather/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import WeatherData
 
-# Register your models here.
+
+class WeatherAdmin(admin.ModelAdmin):
+    list_display = ['id', 'region', 'weather_parameter']
+    search_fields = ['region', 'weather_parameter']
+    list_filter = ['weather_parameter', 'region']
+
+
+admin.site.register(WeatherData, WeatherAdmin)
