504202ad70fd95cc71c91a67a32baecb90576a69
Rushikesh Bhavsar <rushikesh.bhavsar@bajajfinserv.in>
2023-06-18 12:12:29 +0530

    Pagination and basic list method definition added.

--- FarmSetu/settings.py
+++ FarmSetu/settings.py
@@ -189,4 +189,9 @@ LOGGING = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
+}
+
 WEATHER_URL = "https://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/<weather_parameter>/date/<region>.txt"
--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .models import WeatherData
 from .serializer import WeatherSerializer
+from rest_framework.response import Response
 
 
 class WeatherViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = WeatherData.objects.all()[:10]
+    queryset = WeatherData.objects.all()
     serializer_class = WeatherSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        region = kwargs['region']
+        return self.queryset.filter(region__in=region, weather_parameter='Tmax')
+
+    def list(self, request, *args, **kwargs):
+        instance = self.get_queryset(**request.query_params)
+        if instance:
+            msg = "Record details fetched successfully."
+            serializer = self.get_serializer(instance, many=True)
+            data_content = {'code': 100, 'msg': msg, 'count': instance.count(), 'result': serializer.data}
+        else:
+            msg = "No Data Present"
+            data_content = {'code': 100, 'msg': msg, 'count': 0, 'result': []}
+        return Response(data=data_content, status=200)
