47f386e45c23303f7b74e2a20069d1440774c03b
Rushikesh Bhavsar <rushikesh.bhavsar@bajajfinserv.in>
2023-06-20 11:38:34 +0530

    Added Management command to load data from website.

---
+++ weather/management/commands/load_data_from_site.py
@@ -0,0 +1,18 @@
+import logging
+from django.core.management.base import BaseCommand
+from weather.utils.get_weather_data import get_region_wise_data
+
+
+class Command(BaseCommand):
+    help = 'To Load Data from Website.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--load_data', type=str, help='Parameter that will decide to load the data or not')
+
+    def handle(self, *args, **kwargs):
+        if kwargs['load_data'] == 'load':
+            get_region_wise_data()
+            self.stdout.write(f"Data Loading completed.")
+        else:
+            self.stdout.write(f"Data will not be loaded. Pass input parameter as 'load'")
+
